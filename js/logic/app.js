e1ff8137435f73d5e866ab2d1751c01d5ec85def
Save <>
2018-06-24 23:32:26 -0600

    Add netManager.js

--- js/logic/app.js
+++ js/logic/app.js
@@ -9,6 +9,7 @@ class App {
 	constructor() {
 		this.dataManager = new DataManager();
 		this.navManager = new NavManager(this);
+		this.netManager = new NetManager(this);
 		this.navManager.init();
 	}
 }
---
+++ js/logic/netManager.js
@@ -0,0 +1,15 @@
+/**
+* @name NetManager
+* @extends
+* @file netManager.js
+* @author Esteban Padilla <ep@estebanpadilla.com>
+* @version 1.0.0
+*/
+class NetManager {
+	/**
+	* @param {data type} name - description.
+	*/
+	constructor() {
+
+	}
+}
